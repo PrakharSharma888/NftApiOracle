8cbdefd3f55af482ef58402909ba4056e174f60f
Prakhar Sharma <prakharentp2002@gmail.com>
2023-01-25 17:55:32 +0530

    all done

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,9 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
 
-contract NFTApi {
-    function getNfts(address _user) view public returns(address){
-        return _user;
-        }
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTApi is Ownable{
+
+    address nftToken_address;
+  
+    event getNftsEvent(
+        address _user
+    );
+
+    function getNfts(address _user) public {
+        emit getNftsEvent(
+            _user
+        );
+
+    }
+    
+    function nftStore(address _nftToken_address) public onlyOwner returns(bool){
+        nftToken_address = _nftToken_address;
+        return true;
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const hre = require("hardhat");
 const Moralis = require("moralis").default;
-const { EvmChain } = require('@moralisweb3/evm-utils');
+const { EvmChain } = require('@moralisweb3/common-evm-utils');
+const { json } = require("hardhat/internal/core/params/argumentTypes");
 
 async function main() {
 
@@ -14,26 +15,43 @@ async function main() {
     "Address :", _api.address
   );
 
-  const call = await _api.getNfts("0x5aAB360f4eEC9C823175711d22D7D0C920D4481a")
-  console.log(call)
+  const transactionResponse = await _api.getNfts("0x1234567890123456789012345678901234567890")
+  const transactionReceipt = await transactionResponse.wait()
+  // console.log(transactionReceipt.events[0])
+  // console.log(transactionReceipt.events[0].args._user)
+  //const call = await _api.getNfts("0x5aAB360f4eEC9C823175711d22D7D0C920D4481a")
+  //console.log(call)
   try {
-    const address = '0x1234567890123456789012345678901234567890';
+    const address = transactionReceipt.events[0].args._user;
 
     const chain = EvmChain.ETHEREUM;
 
     Moralis.start({
-        apiKey: 'IwGrsP6BGVVlWrbNvxGQIGYwG4rLfwPAlVBsBipKPDgIeJxX2A3W9Khr9lPKfpn9',
-        // ...and any other configuration
+        apiKey: 'XcYQc1b4QtGOUaMrHDdQUFRm1R9rF1jNfTk86YuNyDPy4WhA2fsw52X56XFaOehm',
     })
 
     const response = await Moralis.EvmApi.nft.getWalletNFTs({
+
         address,
         chain,
     });
-    const nftName = response.result[0].name
-    console.log(nftName);
+    // const nftName = response.result[1].name
+    // console.log(nftName);
+   // const n = response.result[12].name
+   const jsonData = response.toJSON()
+    console.log("Data: ",jsonData.result[0].token_address);
+    const returnValue = jsonData.result[0].token_address
+
+    const receiptPromise = _api.nftStore(returnValue);
+    receiptPromise.then((receipt) => {
+      console.log({ receipt });
+    });
+
+    //console.log(n);
   } catch (e) {
     console.error(e);
+
+
 }
 }
 
